ad3d8ad1e42435c69951483ae28f6b991054c305
DanielB <d.banasiewicz@pdo.com.pl>
2021-11-12 19:17:41 +0100

    First project with router done;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => {
+    return (
+        <h1>Strona startowa</h1>
+    );
+}
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt</h1>;
+const ErrorPage = () => <h1>Taka strona nie istnieje</h1>;
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <header>
+                        <nav>
+                            <ul>
+                                {/*<li><a href="/">Start</a></li>*/}
+                                {/*<li><a href="/news">Aktualności</a></li>*/}
+                                {/*<li><a href="/contact">Kontakt</a></li>*/}
+                                <li>
+                                    <NavLink to="/"
+                                             exact
+                                             className="home_selected">Start</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink exact
+                                             to="/news"
+                                             className="news_selected">Aktualności</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink exact
+                                             to="/contact"
+                                             className="contact_selected">Kontakt</NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <section>
+                        <Routes>
+                                <Route path="/"
+                                       exact
+                                       element={<Home/>}/>
+                                <Route path="/news"
+                                       element={<News/>}/>
+                                <Route path="/contact"
+                                       element={<Contact/>}/>
+                                <Route path="*"
+                                       element={<ErrorPage/>}/>
+                        </Routes>
+                    </section>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
